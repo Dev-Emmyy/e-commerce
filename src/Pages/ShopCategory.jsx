5b2f7cc9b4cc9c641398d37250cdca91cb5dc58e
Gunna9999 <emmanuelugochukwu2000@gmail.com>
2023-11-09 00:43:28 +0100

    commit

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,14 +1,30 @@
 import React, { useContext } from 'react';
 import "./ShopCategory.css";
 import { ShopContext } from '../Context/ShopContext';
+import dropdown_icon from "../Components/Assests/dropdown_icon.png";
 
 
-const ShopCategory = () => {
-  const {all_product} = useContext(ShopContext)
+const ShopCategory = (props) => {
+  const {all_product} = useContext(ShopContext);
   
   return (
     <div className='shop-category'>
+      <img src={props.banner} alt='banner' width={1000} height={250} />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt='dropdown_icon'/>
+       </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_product.map((item,i) => {
+          if (props.category === item.category) {
 
+          }
+        })}
+      </div>
     </div>
   )
 }
