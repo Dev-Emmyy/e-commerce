1b31fffc47dcce25e975c12ac095285f842d67ac
Gunna9999 <emmanuelugochukwu2000@gmail.com>
2023-11-10 01:27:14 +0100

    commit

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -2,14 +2,14 @@ import React, { useContext } from 'react';
 import "./ShopCategory.css";
 import { ShopContext } from '../Context/ShopContext';
 import dropdown_icon from "../Components/Assests/dropdown_icon.png";
-
+import Item from '../Components/Item/Item';
 
 const ShopCategory = (props) => {
   const {all_product} = useContext(ShopContext);
   
   return (
     <div className='shop-category'>
-      <img src={props.banner} alt='banner' width={1000} height={250} />
+      <img className='shop-category-banner' src={props.banner} alt='banner' />
       <div className="shopcategory-indexSort">
         <p>
           <span>Showing 1-12</span> out of 36 products
@@ -21,9 +21,15 @@ const ShopCategory = (props) => {
       <div className="shopcategory-products">
         {all_product.map((item,i) => {
           if (props.category === item.category) {
-
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+          }
+          else {
+            return null;
           }
         })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore More
       </div>
     </div>
   )
